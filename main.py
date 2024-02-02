c9783ab5477b07d2abf0a890d96b97e21aa16a37
uliazaman02 <ulia.zaman02@gmail.com>
2024-02-01 20:53:45 -0800

    moveDown

--- main.py
+++ main.py
@@ -54,6 +54,17 @@ def moveUp(game_board: [[int, ], ]) -> [[int, ], ]:
                     game_board[row-1][col] += game_board[row][col]
                     game_board[row][col] = 0
 
+def moveDown(game_board: [[int, ], ]) -> [[int, ], ]:
+    for col in range(len(game_board)):
+        for row in range((len(game_board))-1):
+            if game_board[row][col] != 0:
+                if game_board[row+1][col] == 0:
+                    game_board[row+1][col] += game_board[row][col]
+                    game_board[row][col] = 0
+                elif game_board[row][col] == game_board[row+1][col]:
+                    game_board[row+1][col] += game_board[row][col]
+                    game_board[row][col] = 0
+
 
 if __name__ == "__main__":
     print("hi")
