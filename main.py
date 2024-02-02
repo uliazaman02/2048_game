0b7cf0f083c07c632b2210d2bfd70e78781578c8
uliazaman02 <ulia.zaman02@gmail.com>
2024-02-01 20:33:53 -0800

    some old code

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 def moveleft(game_board: [[int, ], ]) -> [[int, ], ]:
+    for col in reversed(range (1, len(game_board))):
+        for row in range(len(game_board)):
+            if game_board[row][col] != 0:
+                if game_board[row][col-1] == game_board[row][col]: # is the if/elif necessary?
+                    game_board[row][col-1] += game_board[row][col]
+                    game_board[row][col] = 0
+                elif game_board[row][col-1] == 0:
+                    game_board[row][col-1] += game_board[row][col]
+                    game_board[row][col] = 0
+    for col in reversed(range (1, len(game_board))): # use previous nested for loop?
+        for row in range(len(game_board)):
+            if game_board[row][col] != 0:
+                if game_board[row][col-1] == 0:
+                    game_board[row][col-1] += game_board[row][col]
+                    game_board[row][col] = 0
     print("bye")
 
 if __name__ == "__main__":
