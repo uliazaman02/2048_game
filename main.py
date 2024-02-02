32946f7e34cab73c18eec84b1f6337b40b65e2b5
uliazaman02 <ulia.zaman02@gmail.com>
2024-02-01 20:39:38 -0800

    moveRight

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def moveleft(game_board: [[int, ], ]) -> [[int, ], ]:
+"""
+TO-DO: 
+- need to clarify concise moves to avoid repition
+- add some comments and function definitions
+"""
+
+def moveLeft(game_board: [[int, ], ]) -> [[int, ], ]:
     for col in reversed(range (1, len(game_board))):
         for row in range(len(game_board)):
             if game_board[row][col] != 0:
@@ -16,6 +22,24 @@ def moveleft(game_board: [[int, ], ]) -> [[int, ], ]:
                     game_board[row][col] = 0
     print("bye")
 
+def moveRight(game_board: [[int, ], ]) -> [[int, ], ]:
+    for col in range((len(game_board)) - 1):
+        for row in range(len(game_board)):
+            if game_board[row][col] != 0:
+                if game_board[row][col+1] == 0:
+                    game_board[row][col+1] += game_board[row][col]
+                    game_board[row][col] = 0
+                elif game_board[row][col] == game_board[row][col+1]:
+                    game_board[row][col+1] += game_board[row][col]
+                    game_board[row][col] = 0
+    for col in reversed(range((len(game_board))-1)):
+        for row in range(len(game_board)):
+            if game_board[row][col] != 0:
+                if game_board[row][col+1] == 0:
+                    game_board[row][col+1] += game_board[row][col]
+                    game_board[row][col] = 0
+
+
 if __name__ == "__main__":
     print("hi")
-    moveleft([[]])
+    moveLeft([[]])
